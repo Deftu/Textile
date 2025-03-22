13c40c25159d550fdaa83b54b54588a1dfa50b98
Deftu <me@deftu.dev>
2025-03-22 18:02:02 +0200

    Add foojay-resolver-convention to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,6 +24,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.+")
+}
+
 rootProject.name = extra["project.name"]?.toString() ?: throw IllegalStateException("project.name is not defined")
 
 // Setup Minecraft
