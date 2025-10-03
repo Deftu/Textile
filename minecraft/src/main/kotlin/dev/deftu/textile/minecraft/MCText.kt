8e19b3b2dba1941ae06a82778ce169fcb14f14e2
Deftu <me@deftu.dev>
2025-10-03 12:23:35 +0200

    Proxy `Text` factory functions in `MCText`

--- minecraft/src/main/kotlin/dev/deftu/textile/minecraft/MCText.kt
+++ minecraft/src/main/kotlin/dev/deftu/textile/minecraft/MCText.kt
@@ -1,10 +1,27 @@
 package dev.deftu.textile.minecraft
 
+import dev.deftu.textile.MutableText
 import dev.deftu.textile.Text
+import dev.deftu.textile.TextStyle
 import net.minecraft.text.Text as VanillaText
 import java.util.Optional
 
 public object MCText {
+    @JvmStatic
+    public fun empty(): MutableText {
+        return Text.empty()
+    }
+
+    @JvmStatic
+    public fun literal(content: String): MutableText {
+        return Text.literal(content)
+    }
+
+    @JvmStatic
+    public fun literal(content: String, style: TextStyle): MutableText {
+        return Text.literal(content, style)
+    }
+
     @JvmStatic
     public fun wrap(text: VanillaText): Text {
         val result = Text.literal(buildString {
