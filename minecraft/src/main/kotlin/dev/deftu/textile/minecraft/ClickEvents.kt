6db3dee31c6b3f2ba1e4e7623db7fc1e35312e27
Deftu <me@deftu.dev>
2025-10-03 12:35:14 +0200

    Add conversion extensions

--- minecraft/src/main/kotlin/dev/deftu/textile/minecraft/ClickEvents.kt
+++ minecraft/src/main/kotlin/dev/deftu/textile/minecraft/ClickEvents.kt
@@ -37,30 +37,6 @@ public object ClickEvents {
 
     @JvmStatic
     public fun convert(event: ClickEvent): VanillaClickEvent {
-        //#if MC >= 1.21.5
-        //$$ return when (event) {
-        //$$     is ClickEvent.OpenUrl -> VanillaClickEvent.OpenUrl(event.uri)
-        //$$     is ClickEvent.OpenFile -> VanillaClickEvent.OpenFile(event.path)
-        //$$     is ClickEvent.RunCommand -> VanillaClickEvent.RunCommand(event.command)
-        //$$     is ClickEvent.SuggestCommand -> VanillaClickEvent.SuggestCommand(event.command)
-        //$$     is ClickEvent.ChangePage -> VanillaClickEvent.ChangePage(event.page)
-        //$$     is ClickEvent.CopyToClipboard -> VanillaClickEvent.CopyToClipboard(event.value)
-            //#if MC >= 1.21.6
-            //$$ is ClickEvent.ShowDialog -> VanillaClickEvent.ShowDialog(event.dialog)
-            //$$ is ClickEvent.Custom -> VanillaClickEvent.Custom(event.identifier, event.payload)
-            //#endif
-        //$$ }
-        //#else
-        return when (event) {
-            is ClickEvent.OpenUrl -> VanillaClickEvent(VanillaClickEvent.Action.OPEN_URL, event.uri.toString())
-            is ClickEvent.OpenFile -> VanillaClickEvent(VanillaClickEvent.Action.OPEN_FILE, event.path)
-            is ClickEvent.RunCommand -> VanillaClickEvent(VanillaClickEvent.Action.RUN_COMMAND, event.command)
-            is ClickEvent.SuggestCommand -> VanillaClickEvent(VanillaClickEvent.Action.SUGGEST_COMMAND, event.command)
-            is ClickEvent.ChangePage -> VanillaClickEvent(VanillaClickEvent.Action.CHANGE_PAGE, event.page.toString())
-            //#if MC >= 1.16.5
-            is ClickEvent.CopyToClipboard -> VanillaClickEvent(VanillaClickEvent.Action.COPY_TO_CLIPBOARD, event.value)
-            //#endif
-        }
-        //#endif
+        return event.asVanilla()
     }
 }
