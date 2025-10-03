994dea088564104d9cf17517a282bb5d32104267
Deftu <me@deftu.dev>
2025-10-03 12:23:56 +0200

    Update 1.18.2 `MCText` overload too

--- minecraft/versions/1.18.2-fabric/src/main/kotlin/dev/deftu/textile/minecraft/MCText.kt
+++ minecraft/versions/1.18.2-fabric/src/main/kotlin/dev/deftu/textile/minecraft/MCText.kt
@@ -1,11 +1,46 @@
 package dev.deftu.textile.minecraft
 
+import dev.deftu.textile.MutableText
 import dev.deftu.textile.Text
+import dev.deftu.textile.TextStyle
 import net.minecraft.text.LiteralText
 import net.minecraft.text.Text as VanillaText
-import java.util.Optional
 
 public object MCText {
+    @JvmStatic
+    public fun empty(): MutableText {
+        return Text.empty()
+    }
+
+    @JvmStatic
+    public fun literal(content: String): MutableText {
+        return Text.literal(content)
+    }
+
+    @JvmStatic
+    public fun literal(content: String, style: TextStyle): MutableText {
+        return Text.literal(content, style)
+    }
+
+    @JvmStatic
+    public fun translatable(key: String, vararg replacements: Any): MutableText {
+        return Text.of(TranslatableTextContent(key, replacements))
+    }
+
+    @JvmStatic
+    public fun translatable(key: String, fallback: String?, vararg replacements: Any): MutableText {
+        return Text.of(TranslatableTextContent(key, fallback, replacements))
+    }
+    @JvmStatic
+    public fun translatable(key: String, style: TextStyle, vararg replacements: Any): MutableText {
+        return Text.of(TranslatableTextContent(key, replacements), style)
+    }
+
+    @JvmStatic
+    public fun translatable(key: String, fallback: String?, style: TextStyle, vararg replacements: Any): MutableText {
+        return Text.of(TranslatableTextContent(key, fallback, replacements), style)
+    }
+
     @JvmStatic
     public fun wrap(text: VanillaText): Text {
         val result = Text.literal(text.string)
