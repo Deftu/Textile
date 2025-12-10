d98dcfe2439ed7c80b5d53196c796284ba90630e
Deftu <me@deftu.dev>
2025-12-10 13:35:44 +0200

    Add `EMPTY` constant to TextStyleBuilder

--- src/commonMain/kotlin/dev/deftu/textile/TextStyleBuilder.kt
+++ src/commonMain/kotlin/dev/deftu/textile/TextStyleBuilder.kt
@@ -1,5 +1,11 @@
 package dev.deftu.textile
 
+import kotlin.jvm.JvmField
+
 public fun interface TextStyleBuilder {
+    public companion object {
+        @JvmField public val EMPTY: TextStyleBuilder = TextStyleBuilder { TextStyle.EMPTY }
+    }
+
     public fun build(): TextStyle
 }
